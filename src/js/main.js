ab288e74b5985f514d57c6379d988fa5494606cb
Mbueno992 <mbueno_92@hotmail.com>
2024-01-19 13:02:40 +0100

    add Local Storage and favorites

--- src/js/main.js
+++ src/js/main.js
@@ -7,14 +7,18 @@ const resetBtn = document.querySelector('.js-resetBtn');
 const favSection = document.querySelector('.js-favSection');
 const resultSection = document.querySelector('.js-resultSection');
 
-let animeSeries = [];
+let resultSeries = [];
+let favSeries = [];
 let animeSearch = '';
 
-const addSearchResult = () => {
-  resultSection.innerHTML = '';
-  for (const anime of animeSeries) {
+//Funciones de búsqueda
+
+const addSearchResult = (series, container) => {
+  for (const anime of series) {
     const article = document.createElement('article');
-    resultSection.appendChild(article);
+    article.setAttribute('class', 'main__result--series js-serie');
+    article.setAttribute('id', anime.mal_id);
+    container.appendChild(article);
     const seriesImg = document.createElement('img');
     article.appendChild(seriesImg);
     const seriesTitle = document.createElement('h4');
@@ -23,7 +27,17 @@ const addSearchResult = () => {
     seriesImg.setAttribute('alt', anime.title);
     const title = document.createTextNode(anime.title);
     seriesTitle.appendChild(title);
+    if (series === favSeries || series === refresh) {
+      article.setAttribute('class', 'main__fav--series');
+      const span = document.createElement('span');
+      const text = document.createTextNode('X');
+      span.setAttribute('class', 'quitList');
+      article.appendChild(span);
+      span.appendChild(text);
+    }
   }
+  listenerFavoriteSerie();
+  //https://via.placeholder.com/210x295/c1c1c3/666666/?text=TV.
 };
 
 const chargeDataApi = (event) => {
@@ -31,8 +45,9 @@ const chargeDataApi = (event) => {
   fetch(`${animeSearch}`)
     .then((response) => response.json())
     .then((listSeries) => {
-      animeSeries = listSeries.data;
-      addSearchResult(animeSeries);
+      resultSeries = listSeries.data;
+      resultSection.innerHTML = '';
+      addSearchResult(resultSeries, resultSection);
     })
     .catch((error) => console.log('Error', error));
 };
@@ -42,7 +57,34 @@ searchBtn.addEventListener('click', chargeDataApi);
 const handleSearchAnime = () => {
   const userSearch = inputSearch.value.toLowerCase();
   animeSearch = `https://api.jikan.moe/v4/anime?q=${userSearch}`;
-  console.log(animeSearch);
 };
 
 inputSearch.addEventListener('input', handleSearchAnime);
+
+//Funciones de Favoritos
+
+const handleFavoriteSerie = (event) => {
+  const serie = event.currentTarget;
+  const id = parseInt(event.currentTarget.id);
+  const serieIndex = resultSeries.find((fav) => id === fav.mal_id);
+  const serieId = favSeries.findIndex((fav) => fav.mal_id === id);
+  if (serieId === -1) {
+    favSeries.push(serieIndex);
+    serie.setAttribute('class', 'main__result--series js-serie favorite');
+  }
+
+  favSection.innerHTML = '';
+  localStorage.setItem('favSeries', JSON.stringify(favSeries));
+
+  addSearchResult(favSeries, favSection);
+};
+
+const listenerFavoriteSerie = () => {
+  const serie = document.querySelectorAll('.js-serie');
+  serie.forEach((option) =>
+    option.addEventListener('click', handleFavoriteSerie)
+  );
+};
+
+const refresh = JSON.parse(localStorage.getItem('favSeries'));
+addSearchResult(refresh, favSection);
