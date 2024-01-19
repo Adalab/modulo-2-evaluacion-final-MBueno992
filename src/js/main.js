fc5f8cd67f5c5bc46902f75c3111b50424854b31
Mbueno992 <mbueno_92@hotmail.com>
2024-01-19 09:52:20 +0100

    add search function and paint by screen

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-console.log('>> Ready :)');
+//Variables
+const inputSearch = document.querySelector('.js-search');
+const searchBtn = document.querySelector('.js-searchBtn');
+const resetBtn = document.querySelector('.js-resetBtn');
+const favSection = document.querySelector('.js-favSection');
+const resultSection = document.querySelector('.js-resultSection');
+
+let animeSeries = [];
+let animeSearch = '';
+
+const addSearchResult = () => {
+  resultSection.innerHTML = '';
+  for (const anime of animeSeries) {
+    const article = document.createElement('article');
+    resultSection.appendChild(article);
+    const seriesImg = document.createElement('img');
+    article.appendChild(seriesImg);
+    const seriesTitle = document.createElement('h4');
+    article.appendChild(seriesTitle);
+    seriesImg.setAttribute('src', anime.images.webp.image_url);
+    seriesImg.setAttribute('alt', anime.title);
+    const title = document.createTextNode(anime.title);
+    seriesTitle.appendChild(title);
+  }
+};
+
+const chargeDataApi = (event) => {
+  event.preventDefault();
+  fetch(`${animeSearch}`)
+    .then((response) => response.json())
+    .then((listSeries) => {
+      animeSeries = listSeries.data;
+      addSearchResult(animeSeries);
+    })
+    .catch((error) => console.log('Error', error));
+};
+
+searchBtn.addEventListener('click', chargeDataApi);
+
+const handleSearchAnime = () => {
+  const userSearch = inputSearch.value.toLowerCase();
+  animeSearch = `https://api.jikan.moe/v4/anime?q=${userSearch}`;
+  console.log(animeSearch);
+};
+
+inputSearch.addEventListener('input', handleSearchAnime);
