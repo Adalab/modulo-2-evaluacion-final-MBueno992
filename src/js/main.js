630fdf30683cb96f3084cfec8ee65395cdc6bc53
Mbueno992 <mbueno_92@hotmail.com>
2024-01-21 13:31:54 +0100

    RESET btn and new styles

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ const searchBtn = document.querySelector('.js-searchBtn');
 const resetBtn = document.querySelector('.js-resetBtn');
 const favSection = document.querySelector('.js-favSection');
 const resultSection = document.querySelector('.js-resultSection');
+const resetFav = document.querySelector('.js-resetFav');
 
 let resultSeries = [];
 let favSeries = [];
@@ -16,19 +17,29 @@ let animeSearch = '';
 const addSearchResult = (series, container) => {
   for (const anime of series) {
     const article = document.createElement('article');
-    article.setAttribute('class', 'main__result--series js-serie');
+    article.setAttribute('class', 'result__section--series js-serie');
     article.setAttribute('id', anime.mal_id);
     container.appendChild(article);
     const seriesImg = document.createElement('img');
     article.appendChild(seriesImg);
     const seriesTitle = document.createElement('h4');
     article.appendChild(seriesTitle);
-    seriesImg.setAttribute('src', anime.images.webp.image_url);
+    if (
+      anime.images.webp.image_url ===
+      `https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png`
+    ) {
+      seriesImg.setAttribute(
+        'src',
+        `https://placehold.co/210x295/c1c1c3/666666/?text=Imagen\n+no+encontrada`
+      );
+    } else {
+      seriesImg.setAttribute('src', anime.images.webp.image_url);
+    }
     seriesImg.setAttribute('alt', anime.title);
     const title = document.createTextNode(anime.title);
     seriesTitle.appendChild(title);
     if (series === favSeries || series === refresh) {
-      article.setAttribute('class', 'main__fav--series');
+      article.setAttribute('class', 'fav__section--series');
       const span = document.createElement('span');
       const text = document.createTextNode('X');
       span.setAttribute('class', 'quitList js-remove');
@@ -37,7 +48,6 @@ const addSearchResult = (series, container) => {
     }
   }
   listenerFavoriteSerie();
-  //https://via.placeholder.com/210x295/c1c1c3/666666/?text=TV.
 };
 
 const chargeDataApi = (event) => {
@@ -55,7 +65,7 @@ const chargeDataApi = (event) => {
 searchBtn.addEventListener('click', chargeDataApi);
 
 const handleSearchAnime = () => {
-  const userSearch = inputSearch.value.toLowerCase();
+  const userSearch = inputSearch.value;
   animeSearch = `https://api.jikan.moe/v4/anime?q=${userSearch}`;
 };
 
@@ -65,39 +75,46 @@ inputSearch.addEventListener('input', handleSearchAnime);
 
 const handleFavoriteSerie = (event) => {
   const serie = event.currentTarget;
-  const id = parseInt(event.currentTarget.id);
+  const id = parseInt(serie.id);
   const serieIndex = resultSeries.find((fav) => id === fav.mal_id);
   const serieId = favSeries.findIndex((fav) => fav.mal_id === id);
   if (serieId === -1) {
     favSeries.push(serieIndex);
-    serie.setAttribute('class', 'main__result--series js-serie favorite');
+    serie.setAttribute('class', 'result__section--series js-serie favorite');
   }
-
   favSection.innerHTML = '';
-  localStorage.setItem('favSeries', JSON.stringify(favSeries));
-
+  localStorage.setItem('favorites', JSON.stringify(favSeries));
   addSearchResult(favSeries, favSection);
-  handleRemove();
 };
 
+const handleRemove = (event) => {
+  const remove = event.currentTarget;
+  const parent = remove.parentNode;
+  const idRemove = parseInt(parent.id);
+  parent.remove(); //Elimina el artículo del DOM
+
+  const newRefresh = refresh.filter((item) => item.mal_id !== idRemove);
+  localStorage.setItem('favorites', JSON.stringify(newRefresh));
+};
+
+//Escucha los elementos favoritos, se llama al crearse en el DOM en la función addSearchResult
 const listenerFavoriteSerie = () => {
   const serie = document.querySelectorAll('.js-serie');
   serie.forEach((option) =>
     option.addEventListener('click', handleFavoriteSerie)
   );
+  const remove = document.querySelectorAll('.js-remove');
+  remove.forEach((quit) => quit.addEventListener('click', handleRemove));
 };
 
-const refresh = JSON.parse(localStorage.getItem('favSeries'));
-
+const refresh = JSON.parse(localStorage.getItem('favorites'));
 addSearchResult(refresh, favSection);
 
-//BONUS
-const remove = document.querySelector('.js-remove');
-
-const handleRemove = (event) => {
-  const click = event.target;
-  const parent = event.currentTarget.id;
-  console.log(parent);
+//RESET (elimina también LocalStorage)
+const handleReset = () => {
+  resultSection.innerHTML = '';
+  favSection.innerHTML = '';
+  inputSearch.value = '';
+  localStorage.removeItem('favorites');
 };
-
-remove.addEventListener('click', handleRemove);
+resetBtn.addEventListener('click', handleReset);
