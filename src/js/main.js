44b443d0290706ff03ebaa0a0ca1ff7d08a40dd7
Mbueno992 <mbueno_92@hotmail.com>
2024-03-15 13:28:40 +0100

    code improvements made

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,18 @@
 //Variables
 const inputSearch = document.querySelector('.js-search');
 const searchBtn = document.querySelector('.js-searchBtn');
-const resetBtn = document.querySelectorAll('.js-resetBtn');
+const resetBtn = document.querySelector('.js-resetBtn');
 const favSection = document.querySelector('.js-favSection');
 const resultSection = document.querySelector('.js-resultSection');
+const resetFavBtn = document.querySelector('.js-resetFavBtn');
+const noImageUrl =
+  'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png';
+const imageNotFound =
+  'https://placehold.co/210x295/c1c1c3/666666/?text=Imagen+no+encontrada';
 
 let resultSeries = [];
 let favSeries = [];
-let animeSearch = ''; //recoge el valor del input
-
-//BÚSQUEDA
-
-//Añade en el DOM las series
+let animeSearch = '';
 
 const addSearchResult = (series, container) => {
   container.innerHTML = '';
@@ -24,43 +25,35 @@ const addSearchResult = (series, container) => {
     container.appendChild(article);
     const seriesImg = document.createElement('img');
     article.appendChild(seriesImg);
-    if (
-      anime.images.webp.image_url ===
-      `https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png`
-    ) {
-      seriesImg.setAttribute(
-        'src',
-        `https://placehold.co/210x295/c1c1c3/666666/?text=Imagen+no+encontrada`
-      );
-    } else {
-      seriesImg.setAttribute('src', anime.images.webp.image_url);
-    }
+    anime.images.webp.image_url === noImageUrl
+      ? seriesImg.setAttribute('src', imageNotFound)
+      : seriesImg.setAttribute('src', anime.images.webp.image_url);
     seriesImg.setAttribute('alt', anime.title);
     const seriesTitle = document.createElement('h4');
     article.appendChild(seriesTitle);
     const title = document.createTextNode(anime.title);
     seriesTitle.appendChild(title);
-    //Recorre el array y pinta los que están en favSeries
     const isFavorite = favSeries.findIndex(
       (fav) => fav.mal_id === anime.mal_id
     );
     if (isFavorite !== -1) {
       article.classList.add('favorite');
     }
-    // Se crea cuando el parámetro series tiene alguno de estos valores
     if (series === favSeries || series === refresh) {
-      article.setAttribute('class', 'fav__section--series');
+      article.setAttribute('class', 'fav__section--series js-favorite');
       const span = document.createElement('span');
       const text = document.createTextNode('X');
       span.setAttribute('class', 'quitList js-remove');
       article.appendChild(span);
       span.appendChild(text);
     }
+    favSeries.length !== 0
+      ? resetFavBtn.classList.remove('hidden')
+      : resetFavBtn.classList.add('hidden');
   }
   listenerFavoriteSerie();
 };
 
-//Carga los datos desde la API
 const chargeDataApi = (event) => {
   event.preventDefault();
   fetch(`${animeSearch}`)
@@ -74,7 +67,6 @@ const chargeDataApi = (event) => {
 
 searchBtn.addEventListener('click', chargeDataApi);
 
-//Recoge los datos del input para poder generar el enlace de la API
 const handleSearchAnime = () => {
   const userSearch = inputSearch.value;
   animeSearch = `https://api.jikan.moe/v4/anime?q=${userSearch}`;
@@ -82,9 +74,6 @@ const handleSearchAnime = () => {
 
 inputSearch.addEventListener('input', handleSearchAnime);
 
-//FAVORITOS
-
-//Añade a favoritos la serie
 const addFavoriteSerie = (event) => {
   const id = parseInt(event.currentTarget.id);
   const serieIndex = resultSeries.find((fav) => id === fav.mal_id);
@@ -99,7 +88,6 @@ const addFavoriteSerie = (event) => {
   addSearchResult(resultSeries, resultSection);
 };
 
-//Borra la serie clickada en X de favoritos
 const handleRemove = (event) => {
   const parent = event.currentTarget.parentNode;
   const idRemove = parseInt(parent.id);
@@ -112,7 +100,6 @@ const handleRemove = (event) => {
   addSearchResult(resultSeries, resultSection);
 };
 
-//Escucha los elementos favoritos, se llama al crearse en el DOM en la función addSearchResult
 const listenerFavoriteSerie = () => {
   const serie = document.querySelectorAll('.js-serie');
   serie.forEach((option) => option.addEventListener('click', addFavoriteSerie));
@@ -120,7 +107,6 @@ const listenerFavoriteSerie = () => {
   remove.forEach((quit) => quit.addEventListener('click', handleRemove));
 };
 
-//RESET
 const handleReset = (event) => {
   const favBtn = event.currentTarget;
   const removeFav = favBtn.parentNode;
@@ -132,16 +118,13 @@ const handleReset = (event) => {
     resultSection.innerHTML = '';
     inputSearch.value = '';
   }
+  resetFavBtn.classList.add('hidden');
 };
-resetBtn.forEach((reset) => reset.addEventListener('click', handleReset));
-
-//Al cargar la página, si hay datos en el LocalStorage los carga, sino muestra mensaje en consola
+resetFavBtn.addEventListener('click', handleReset);
+resetBtn.addEventListener('click', handleReset);
 
 const refresh = JSON.parse(localStorage.getItem('favorites'));
-if (!refresh) {
-  console.log('No hay datos en el LocalStorage');
-} else {
-  //Agrega elementos de refresh a favSeries
+if (refresh) {
   favSeries = [...refresh];
   addSearchResult(refresh, favSection);
 }
